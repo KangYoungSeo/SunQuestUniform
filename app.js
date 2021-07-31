8ef89f60f1bd84dfa574d7e4cf2aa18f7c72d352
SeungMinSong2929 <lordroshan29@naver.com>
2021-07-31 20:06:07 +0900

    edit app.use

--- app.js
+++ app.js
@@ -12,7 +12,7 @@ var bodyParser = require('body-parser');
 var ejs = require('ejs'); // html 랜더링
 
 
-app.use(static(path.join(__dirname,'assets')));
+app.use(static(path.join(__dirname,'../assets')));
 //app.use(express.static('assets'));
 //app.use('/contact', static(path.join(__dirname,'assets')));
 
