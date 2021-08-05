ad237f5a832db6236e9347b33e11dc1ecd5c0763
SeungMinSong2929 <lordroshan29@naver.com>
2021-08-05 21:40:47 +0900

    fix template

--- template.js
+++ template.js
@@ -9,7 +9,7 @@ module.exports = {
     <meta name="viewport" content="width=device-width, initial-scale=1" />
 
     <link rel="apple-touch-icon" href="/img/apple-icon.png" />
-    <link rel="shortcut icon" href="/img/Clover.ico" />
+    <link rel="shortcut icon" href="/img/icon/clov-16.ico" />
     <link rel="stylesheet" href="/css/bootstrap.min.css" />
     <link rel="stylesheet" href="/css/templatemo.css" />
     <link rel="stylesheet" href="/css/custom.css" />
