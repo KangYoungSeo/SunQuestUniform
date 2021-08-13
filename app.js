168944cbabc80e1bcf807b8916c79140d13a9b7c
강영서 (ZW) <68414594+KangYoungSeo@users.noreply.github.com>
2021-08-14 00:43:03 +0900

    Update app.js

--- app.js
+++ app.js
@@ -434,7 +434,7 @@ connection.connect(function(err){
   if(err) { console.log('Disconnected');throw err;}
   console.log('Connected');
 })
-
+/*
 function handleDisconnect() {
   clearTimeout();
   connection.connect(function(err) {            
@@ -454,7 +454,7 @@ function handleDisconnect() {
   });
 }
 handleDisconnect();
-
+*/
 
 //list page base query
 var listsql = 'select P_ID, LCH_DATE, P_NAME, PRICE, SORT, PATH from product where ';
