f91ace830311ca905cd4979f1af1b1b9f699ae4b
SeungMinSong2929 <lordroshan29@naver.com>
2021-08-13 18:30:02 +0900

    fix app.js

--- app.js
+++ app.js
@@ -439,7 +439,7 @@ function handleDisconnect() {
   connection.connect(function(err) {            
     if(err) {                            
       console.log('error when connecting to db:', err);
-      setTimeout(handleDisconnect, 2000); 
+      setTimeout(handleDisconnect, 20000); 
     }                                   
   });                                 
                                          
